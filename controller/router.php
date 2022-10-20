5c59a557e0b7ad4a87a65b92597213d0d113b415
Christel Rodriguez Perez <magicbaboo@gmail.com>
2022-10-20 14:11:01 +0200

    debug 's' + path router

--- controller/router.php
+++ controller/router.php
@@ -2,43 +2,43 @@
 if (isset($_GET['nav'])) {
     switch ($_GET['nav']) {
         case 'itin1':
-            include("controller/itineraire/itin1Controller.php");
+            include("./controller/itineraire/itin1Controller.php");
             break;
         case 'itin2':
-            include("controller/itineraire/itin2Controller.php");
+            include("./controller/itineraire/itin2Controller.php");
             break;
         case 'itin3':
-            include("controller/itineraire/itin3Controller.php");
+            include("./controller/itineraire/itin3Controller.php");
             break;
         case 'projet':
-            include("controller/projetController.php");
+            include("./controller/projetController.php");
             break;
         case 'galerie':
-            include("controller/galerieController.php");
+            include("./controller/galerieController.php");
             break;
         case 'contact':
-            include("controller/contactController.php");
+            include("./controller/contactController.php");
             break;
         case 'login':
-            include("controller/compte/loginController.php");
+            include("./controller/compte/loginController.php");
             break;
         case 'subscribe':
-            include("controller/compte/subscribeController.php");
+            include("./controller/compte/subscribeController.php");
             break;
         case 'projet':
-            include("controller/projetController.php");
+            include("./controller/projetController.php");
             break;
         case 'logout':
-            include("controller/compte/logoutController.php");
+            include("./controller/compte/logoutController.php");
             break;
         case 'forgot-password':
-            include("controller/compte/forgotpasswordController.php");
+            include("./controller/compte/forgotpasswordController.php");
             break;
         case 'reset-password':
-            include("controller/compte/resetpasswordController.php");
+            include("./controller/compte/resetpasswordController.php");
             break;
     }
 } else {
-    include("controllers/accueilController.php");
+    include("./controller/accueilController.php");
 };
 ?>
